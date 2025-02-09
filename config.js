a30d3b6cf4dd66ff5683a8fde21f81ce7f9d7320
ChampDeepak <sharmajijatin27@gmail.com>
2025-02-09 13:58:16 +0530

    deleted config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    apiBaseUrl: process.env.NODE_ENV === 'production' 
-        ? 'https://careconnbackend.onrender.com'
-        : 'http://localhost:3000'
-        //https://your-backend-service.onrender.com
-};
