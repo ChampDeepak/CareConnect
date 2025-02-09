d8784f2dc95769b1d3cbaf3191400b9ce732abb5
ChampDeepak <sharmajijatin27@gmail.com>
2025-02-09 13:04:07 +0530

    Fixed issue in frontend api calls

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const config = {
     apiBaseUrl: process.env.NODE_ENV === 'production' 
-        ? 'https://your-backend-service.onrender.com'
+        ? 'https://careconnbackend.onrender.com'
         : 'http://localhost:3000'
+        //https://your-backend-service.onrender.com
 };
